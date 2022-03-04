86b27189ce13429584933ad9b5cd20dd192e918b
Ho Zhi Ying Michelle <hozhiying@Hos-MacBook-Pro.local>
2022-03-04 14:16:22 +0800

    new

---
+++ Documents/VTTP/Workshop/Wannarunlocal/src/main/java/iss/edu/sg/Wannarunlocal/WannarunlocalApplication.java
@@ -0,0 +1,13 @@
+package iss.edu.sg.Wannarunlocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WannarunlocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WannarunlocalApplication.class, args);
+	}
+
+}
